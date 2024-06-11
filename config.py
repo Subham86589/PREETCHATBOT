ff37054014bf8280f94e61e4b6b5ce7d7435fd94
Subham86589 <167736439+Subham86589@users.noreply.github.com>
2024-06-11 16:27:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "25355409"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "b9c741ba6b62f492dd0a3a39f7b2c526"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+SUPPORT_GRP = "BILLA_GANG_NTWK"
+UPDATE_CHNL = "BILLA_GANG_NETWORK"
+OWNER_USERNAME = "SUBHHHHHHH07"
 
